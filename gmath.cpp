6c17e16a9df1b58cf153869d2c566d88c20facb1
ZXCroon <zxcroon@gmail.com>
2017-04-29 21:04:16 +0800

    completion of Berstein

--- gmath.cpp
+++ gmath.cpp
@@ -8,3 +8,16 @@ ld det(ld a, ld b, ld c, ld d, ld e, ld f, ld g, ld h, ld i) {
   return a * e * i + b * f * g + c * d * h - c * e * g - b * d * i - a * f * h;
 }
 
+ld Bernstein(int i, int n, ld t) {
+  ld nfact = 1, ifact = 1, nmifact = 1;
+  for (int k = 1; k <= n; ++k) {
+    nfact *= k;
+    if (k == i) {
+      ifact = nfact;
+    }
+    if (k == n - i) {
+      nmifact = nfact;
+    }
+  }
+  return nfact / (ifact * nmifact) * pow(double(t), double(i)) * pow(double(1 - t), double(n - i));
+}
--- gmath.h
+++ gmath.h
@@ -9,4 +9,6 @@
 
 ld det(ld a, ld b, ld c, ld d, ld e, ld f, ld g, ld h, ld i);
 
+ld Bernstein(int i, int n, ld t);
+
 #endif //RAY_TRACING_GMATH_H
